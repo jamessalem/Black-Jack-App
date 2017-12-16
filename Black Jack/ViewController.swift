2b12904c1ec2d44eedfdff5a8a8624c35742465d
James Salem <jamessalem@jamess-air.attlocal.net>
2017-12-16 13:34:53 -0800

    Initial Commit

---
+++ Black Jack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Black Jack
+//
+//  Created by James Salem on 12/16/17.
+//  Copyright © 2017 James Salem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
